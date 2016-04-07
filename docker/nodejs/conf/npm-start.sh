0f812e4541eccb9bc8baf6c5e8c3a4dee7e51609
Sam Phomsopha <samphomsopha@gmail.com>
2016-04-07 14:57:08 -0700

    add docker compose, move node docker to sub directory

---
+++ docker/nodejs/conf/npm-start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#exec echo "STARTING";
+/nodejs/bin/npm install
+/nodejs/bin/npm start
+#exec /usr/local/bin/npm install
+#exec /usr/local/bin/npm start
--- server.js
+++ server.js
@@ -9,3 +9,10 @@ handle["/"] = RQHandler.start;
 handle["/start"] = RQHandler.start;
 handle["/upload"] = RQHandler.upload;
 server.start(router.route, handle);
+/*
+var express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080);*/
